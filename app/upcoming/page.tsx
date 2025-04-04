9bf91431733ba832305805133d03941f1be1cad9
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-04-04 19:13:44 +0900

    deleted donekla

--- app/upcoming/page.tsx
+++ app/upcoming/page.tsx
@@ -57,52 +57,6 @@ export default function UpcomingPage() {
               </div>
             </div>
           </div>
-          <div className="flex justify-center tracking-tighter h-[25%] pt-24">
-            <div className="flex border border-gray-700 rounded-3xl h-[50vh] w-[1400px] mt-24 justify-between items-center bg-slate-700">
-              <div className="">
-                <div className="border border-slate-800 bg-gray-800/20 backdrop-blur-lg rounded-4xl grid grid-cols-2 px-24 place-items-center shadow-lg shadow-slate-700 h-[600px]">
-                  <Image
-                    src={"/dkla2.png"}
-                    alt="d1"
-                    width={1000}
-                    height={1000}
-                    className="object-cover w-auto h-[600px] pr-4"
-                  />
-                  <div className="">
-                    <h1 className="text-red-500">
-                      &mdash;{" "}
-                      <span className="text-white tracking-tighter">
-                        {" "}
-                        About The Event
-                      </span>
-                    </h1>
-                    <div className="">
-                      <p className="text-4xl font-extrabold pb-4">Donekla</p>
-                      <p className="text-9xl font-extrabold tracking-tighter leading-20">
-                        Donekla{" "}
-                        <span className="text-pink-700 text-9xl font-extrabold tracking-tighter">
-                          In Tandem
-                        </span>
-                      </p>
-
-                      <p className="text-gray-400 w-1/2 pt-2 leading-5">
-                        Dionela&apos;s distinctive style and sound continue to
-                        make an impression on the music business. His live
-                        performances highlight his artistic range, ranging from
-                        lively, danceable music to moving ballads.
-                      </p>
-                      <a href="">
-                        <div className="flex items-center space-x-8 pt-8 hover:font-bold">
-                          <p className="text-lg">Buy ticket</p>
-                          <BsArrowUpRightCircle size={25} />
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       <div className="block lg:hidden font-sans">
@@ -141,39 +95,6 @@ export default function UpcomingPage() {
                 highlight his artistic range, ranging from lively, danceable
                 music to moving ballads.
               </p>
-            </div>
-            <div className=" flex justify-center items-center mt-4 md:mt-16">
-              <button className="bg-pink-700 w-full px-8 font-bold md:text-3xl md:py-8 py-2 rounded-full">
-                Buy Ticket
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col justify-center items-center relative">
-          <Image
-            src={"/dkla2.png"}
-            alt="dm"
-            width={1920}
-            height={1920}
-            className="object-cover -z-50 brightness-80"
-          />
-          <div className="border border-gray-500 bg-gray-800/60 md:h-[600px] md:bottom-5 bottom-0 mt-52 md:w-[800px] h-[300px] w-[360px] rounded-4xl backdrop-blur-xl absolute p-6 md:p-14 shadow shadow-slate-400">
-            <div className="flex items-center justify-between">
-              <div className="flex flex-col items-start">
-                <h1 className="text-4xl md:text-9xl font-extrabold">Donekla</h1>
-                <p className="text-md text-gray-300 md:text-4xl">In Tandem</p>
-              </div>
-              <a
-                href="https://www.youtube.com/watch?v=bD9whtdq7dw&list=RDMMbD9whtdq7dw&start_radio=1"
-                target="_blank"
-              >
-                <BsArrowUpRightCircle size={50} />
-              </a>
-            </div>
-            <div>
-              <p className="pt-8 text-2xl md:text-4xl">July 25, 2025</p>
-              <p className="md:text-2xl">New York</p>
-              <p className="text-sm md:text-lg md:pt-8">TBA</p>
             </div>
             <div className=" flex justify-center items-center mt-4 md:mt-16">
               <button className="bg-pink-700 w-full px-8 font-bold md:text-3xl md:py-8 py-2 rounded-full">
